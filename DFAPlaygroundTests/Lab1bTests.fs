1c976fcc780c936dd7ad859eefcfcf11d9c3d6ed
Vladislav Prekel <misterptits@yandex.ru>
2021-02-03 10:17:32 +0700

    asdas

---
+++ DFAPlaygroundTests/Lab1bTests.fs
@@ -0,0 +1,24 @@
+module Lab1bTests
+
+open System
+open NUnit.Framework
+
+let nToStr (n: int) len =
+    let nonull = Convert.ToString(n, 2)
+    (String.init (len - nonull.Length) (fun _ -> "0")) + nonull
+
+let isV (s:string) =
+    let zeros = s |> Seq.map (fun c -> c = '0') |> Seq.length
+    let ones = s |> Seq.map (fun c -> c = '1') |> Seq.length
+    zeros >= 2 && ones = 2
+
+
+[<Test>]
+let test () =
+    let lens = [1..9]
+    let r = lens |> List.collect (fun l -> [0 .. (1 <<< l) - 1] |> List.map (fun n -> nToStr n l))
+    let verify str = Fa.verifyNfa Nfa.Nfa (str |> Seq.map Nfa.charToAlphabet)
+    List.iter (fun i -> Assert.That(verify i, Is.EqualTo(isV i)))
+    
+    
+    Assert.Pass()

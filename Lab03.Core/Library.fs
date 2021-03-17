64d33d176343c44b57978e9b83e350919cc43086
Vladislav Prekel <misterptits@yandex.ru>
2021-03-17 23:25:21 +0700

    option

--- Lab03.Core/Library.fs
+++ Lab03.Core/Library.fs
@@ -3,7 +3,7 @@
 module Fa =
     // P<Q, Σ>
     type Pda<'State, 'InputAlphabet, 'StackAlphabet when 'State: comparison> =
-        { Transition: 'State -> 'InputAlphabet -> 'StackAlphabet -> 'State Set * 'StackAlphabet seq // δ(q, a, X) -> (p, γ)
+        { Transition: 'State -> 'InputAlphabet option -> 'StackAlphabet -> 'State Set * 'StackAlphabet seq // δ(q, a, X) -> (p, γ)
           Initial: 'State // q0
           Final: 'State Set // F
           StackInitial: 'StackAlphabet } // Z0

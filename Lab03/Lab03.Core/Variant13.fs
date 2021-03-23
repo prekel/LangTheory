c91c5002182dec8b804f5b942a499e7b1e67f454
Vladislav Prekel <misterptits@yandex.ru>
2021-03-23 15:38:01 +0700

    started 13

--- Lab03/Lab03.Core/Variant13.fs
+++ Lab03/Lab03.Core/Variant13.fs
@@ -1,2 +1,37 @@
 module Lab03.Core.Variant13
 
+type State =
+    | Q0
+    | Q1
+    | Q2
+    | Q3
+
+type Input =
+    | IA
+    | IB
+    | IC
+
+type Stack =
+    | SA
+    | SB
+    | SC
+    | Z0
+
+let delta (q: State) (a: Input option) (X: Stack) =
+    match q, a, X with
+    | _ -> Set.empty, []
+
+let charToAlphabet =
+    function
+    | 'a' -> Some IA
+    | 'b' -> Some IB
+    | 'c' -> Some IC
+    | _ -> None
+
+open Pda
+
+let pda =
+    { Pda.Transition = delta
+      Initial = Q0
+      Final = Set.singleton Q2
+      StackInitial = Z0 }

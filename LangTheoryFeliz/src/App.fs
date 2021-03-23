e1f9f2247a8e63e3328dab168a7a37400838817f
Vladislav Prekel <misterptits@yandex.ru>
2021-03-23 13:23:01 +0700

    router

--- LangTheoryFeliz/src/App.fs
+++ LangTheoryFeliz/src/App.fs
@@ -1,10 +1,34 @@
 module LangTheoryFeliz.App.App
 
 open Feliz
-
 open LangTheoryFeliz.App
 
+open Feliz.Router
+
+type State = { CurrentUrl: string list }
+type Msg = UrlChanged of string list
+
+let init () = { CurrentUrl = Router.currentUrl () }
+
+let update state (UrlChanged segments) = { state with CurrentUrl = segments }
+
 [<ReactComponent>]
-let HelloWorld () =
-    Html.div [ //Lab01.Lab01()
-               Lab03.Lab03() ]
+let Home () =
+    Html.div [ Html.a [ prop.href (Router.format ("Lab01"))
+                        prop.text "Lab01" ]
+               Html.br []
+               Html.a [ prop.href (Router.format ("Lab03"))
+                        prop.text "Lab03" ] ]
+
+[<ReactComponent>]
+let Router () =
+    let state, dispatch = React.useReducer (update, init ())
+
+    React.router [ router.children [ match state.CurrentUrl with
+                                     | [] -> Home()
+                                     | [ "Lab01" ] -> Lab01.Lab01()
+                                     | [ "Lab03" ] -> Lab03.Lab03()
+                                     | _ -> Html.h1 "Not found" ] ]
+
+[<ReactComponent>]
+let App () = Html.div [ Router() ]
